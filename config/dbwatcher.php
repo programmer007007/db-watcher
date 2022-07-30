9df301e7305601dc37b96655b3b7ebbd91fa53eb
programmer007007 <35029477+programmer007007@users.noreply.github.com>
2022-07-30 11:05:30 +0530

    Bug fixes

--- config/dbwatcher.php
+++ config/dbwatcher.php
@@ -3,10 +3,9 @@
 return [
     "db_name" => env("DB_DATABASE"),
     "tables_to_track" => [
-        ["table" => "bingo_venues", "column" => ["venue_location"], "track_event" => ["insert", "update"]]
-        // table is required | column if empty will track all columns | track_event is optional | track_event is an array of events to track if empty will track all events
+        ["table" => "supplier_master", "column" => ["supplier_name", "qnty"], "track_event" => ["insert", "update", "delete"]]
     ],
-    "log_table_name" => 'strongbond_log',
+    "log_table_name" => 'sb_log',
     "common_columns_to_ignore" => ["created_at", "updated_at", "deleted_at"]
 ];
 
